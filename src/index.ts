d9cceb7e4fd62f6dd64c095c9f699a92d6734780
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-04 18:53:48 -0300

    Aviso de funcionamento

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use(cors());
 app.use("/pessoas", pessoasRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API de Pessoas está funcionando!");
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
